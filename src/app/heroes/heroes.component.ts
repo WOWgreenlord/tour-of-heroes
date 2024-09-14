2d2a1b2d10043898c561368acfcc6fb9be781863
Misha Dyakov <dock.misha@gmail.com>
2024-09-14 10:24:10 +0300

    add: new component hero-detail

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { Hero } from '../hero';
-import { UpperCasePipe } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+// import { FormsModule } from '@angular/forms';
+
 import { HEROES } from '../mock-heroes';
+import { Hero } from '../hero';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [UpperCasePipe, FormsModule],
+  imports: [HeroDetailComponent],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
 })
