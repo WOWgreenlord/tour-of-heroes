0e975f67865701fe4ddcc52c1583a55497eced46
Misha Dyakov <dock.misha@gmail.com>
2024-09-14 11:59:23 +0300

    add: messages component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 import { HeroesComponent } from "./heroes/heroes.component";
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroesComponent, HeroDetailComponent],
+  imports: [RouterOutlet, HeroesComponent, HeroDetailComponent, MessagesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
